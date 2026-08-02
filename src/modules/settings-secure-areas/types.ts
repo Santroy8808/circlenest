16aac11ccb410f408779e8ae299f1ab5c3d677bc
Santroy8808 <mike@santroy.com>
2026-08-02 06:37:10 -0700

    Polish Android app installation page

--- src/modules/settings-secure-areas/types.ts
+++ src/modules/settings-secure-areas/types.ts
@@ -3,5 +3,5 @@ export type SettingsCard = {
   description: string;
   href: string;
   sensitive: boolean;
-  badge: "Profile" | "Security" | "Account" | "Rules" | "Preferences" | "Invites" | "Help" | "Conduct" | "Roadmap";
+  badge: "Profile" | "Security" | "Account" | "Rules" | "Preferences" | "Invites" | "Help" | "Conduct" | "Roadmap" | "Apps";
 };
